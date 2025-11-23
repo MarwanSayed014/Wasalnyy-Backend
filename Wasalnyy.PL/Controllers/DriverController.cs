ee7c1c4f3db7959e7be44eaa17d0966ecada9fe7
mahmoudsameh201333 <midomahmoud201333@yahoo.com>
2025-11-23 05:14:07 +0200

    added update for rider and driver add driver rider home page and admin now can suspend accounts

--- Wasalnyy.PL/Controllers/DriverController.cs
+++ Wasalnyy.PL/Controllers/DriverController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using Wasalnyy.BLL.DTO.Driver;
 using Wasalnyy.BLL.DTO.Trip;
 using Wasalnyy.DAL.Entities;
 
@@ -22,39 +22,33 @@ namespace Wasalnyy.PL.Controllers
             _tripService = tripService;
         }
 
+   
+
         [HttpPost("SetAsAvailable")]
         public async Task<IActionResult> SetAsAvailableAsync([FromBody] Coordinates coordinate)
         {
             var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            if (driverId == null)
-                return Unauthorized();
+            if (driverId == null) return Unauthorized();
 
             await _driverService.SetDriverAvailableAsync(driverId, coordinate);
             return Ok();
-
         }
 
         [HttpPost("UpdateLocation")]
         public async Task<IActionResult> UpdateLocationAsync([FromBody] Coordinates coordinate)
         {
             var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            if (driverId == null)
-                return Unauthorized();
+            if (driverId == null) return Unauthorized();
 
             await _driverService.UpdateLocationAsync(driverId, coordinate);
             return Ok();
-
         }
 
         [HttpPost("AcceptTrip")]
         public async Task<IActionResult> AcceptTripAsync([FromBody] Guid tripId)
         {
             var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            if (driverId == null)
-                return Unauthorized();
+            if (driverId == null) return Unauthorized();
 
             await _tripService.AcceptTripAsync(driverId, tripId);
             return Ok();
@@ -64,9 +58,7 @@ namespace Wasalnyy.PL.Controllers
         public async Task<IActionResult> StartTripAsync([FromBody] Guid tripId)
         {
             var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            if (driverId == null)
-                return Unauthorized();
+            if (driverId == null) return Unauthorized();
 
             await _tripService.StartTripAsync(driverId, tripId);
             return Ok();
@@ -76,12 +68,82 @@ namespace Wasalnyy.PL.Controllers
         public async Task<IActionResult> EndTripAsync([FromBody] Guid tripId)
         {
             var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            if (driverId == null)
-                return Unauthorized();
+            if (driverId == null) return Unauthorized();
 
             await _tripService.EndTripAsync(driverId, tripId);
             return Ok();
+        }
+
+     
+
+        [HttpPut("UpdateInfo")]
+        public async Task<IActionResult> UpdateDriverAsync([FromBody] DriverUpdateDto driverUpdate)
+        {
+            var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (driverId == null) return Unauthorized();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _driverService.UpdateDriverInfo(driverId, driverUpdate);
+            if (!result)
+                return NotFound(new { Message = "Driver not found." });
+
+            return Ok(new { Message = "Driver information updated successfully." });
+        }
+
+       
+        [HttpGet("Vehicle")]
+        public async Task<IActionResult> GetVehicleInfoAsync()
+        {
+            var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (driverId == null) return Unauthorized();
+
+            var vehicle = await _driverService.GetDriverVehicleInfoAsync(driverId);
+            if (vehicle == null)
+                return NotFound(new { Message = "Vehicle info not found." });
+
+            return Ok(vehicle);
+        }
+
+        [HttpGet("Status")]
+        public async Task<IActionResult> GetStatusAsync()
+        {
+            var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (driverId == null) return Unauthorized();
+
+            var status = await _driverService.GetDriverStatusAsync(driverId);
+            return Ok(status);
+        }
+
+        [HttpGet("ProfileImage")]
+        public async Task<IActionResult> GetProfileImageAsync()
+        {
+            var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (driverId == null) return Unauthorized();
+
+            var image = await _driverService.DriverProfileImageAsync(driverId);
+            return Ok(image);
+        }
+
+        [HttpGet("Name")]
+        public async Task<IActionResult> GetDriverNameAsync()
+        {
+            var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (driverId == null) return Unauthorized();
+
+            var name = await _driverService.DriverNameAsync(driverId);
+            return Ok(name);
+        }
+
+        [HttpGet("CompletedTripsCount")]
+        public async Task<IActionResult> GetTotalCompletedTripsAsync()
+        {
+            var driverId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (driverId == null) return Unauthorized();
+
+            var tripsCount = await _driverService.GetTotalCompletedTripsAsync(driverId);
+            return Ok(tripsCount);
         }
     }
 }
